c2fefbbb7a5ff33125b8cbe5dd14ee8bd377bc90
Alexey Patosin <apatosin@yandex-team.ru>
2015-02-27 00:03:11 +0300

    sample of usages updated

--- LRUCache/main.m
+++ LRUCache/main.m
@@ -9,33 +9,35 @@
 #import <Foundation/Foundation.h>
 #import "LRUCache.h"
 
-// LRU Cache sample
-
-// initial state for capacity 3
-//      -,-,-
-// add 1
-//      1,-,-
-// add 2
-//      2,1,-
-// add 3
-//      3,2,1
-// add 4
-//      4,3,2
-// request 2
-//      2,4,3
-
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
 
+        // initial state of cache with capacity 3
+        //      -,-,-
         LRUCache *cache = [[LRUCache alloc] initWithCapacity:3];
         
+        // add 1
+        //      1,-,-
         [cache setObject:@"1" forKey:@"1"];
+
+        // add 2
+        //      2,1,-
         [cache setObject:@"2" forKey:@"2"];
+        
+        // add 3
+        //      3,2,1
         [cache setObject:@"3" forKey:@"3"];
+        
+        // add 4
+        //      4,3,2       (1 removed as least recently used)
         [cache setObject:@"4" forKey:@"4"];
 
+        // request 2
+        //      2,4,3       (2 goes to the top as most recently used)
         NSLog(@"%@", [cache objectForKey:@"2"]);
 
+        // add 5
+        //      5,2,4       (3 removed as least recently used)
         [cache setObject:@"5" forKey:@"5"];
     
     }
